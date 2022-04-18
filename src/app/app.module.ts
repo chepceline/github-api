550d2cb4791be06a7919d85902ff6913dc5a3e58
chepceline <chepceline25@gmail.com>
2022-04-18 23:37:33 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { GitRoutingModule } from './git-routing.module'; // // Imports Custom Routing Module GitRoutingModule from src/app/git-routing.module.ts
 import { AppComponent } from './app.component';
+import { GitFormComponent } from './git-form/git-form.component'; // Imports GitFormComponent from src/app/git-form/git-form.component
+import { GitSearchResultsComponent } from './git-search-results/git-search-results.component';
+import { SearchGithubService } from './search-github.service'; // Imports SearchGithubService from src/app/search-github.service.ts
+import { DateFormatPipe } from './date-format.pipe'; // Imports DateFormatPipe from src/app/date-format.pipe
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitFormComponent,
+    GitSearchResultsComponent,
+    DateFormatPipe,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    GitRoutingModule
   ],
-  providers: [],
+  providers: [SearchGithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
